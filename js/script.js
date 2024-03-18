7b54a54b85afbe27319a61abce6ca397a9f0e75a
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2024-03-18 13:51:09 +0200

    add

--- js/script.js
+++ js/script.js
@@ -53,3 +53,73 @@ $('.select').each(function() {
     });
 });
 
+// $(document).ready(function(){
+//     $(window).on("scroll", function() {
+//         $(".advantages__item-img-descr-list").each(function() {
+//             if ($(this).offset().top < $(window).scrollTop() + $(window).height()) {
+//                 $(this).find('h5').each(function() {
+//                     var $this = $(this);
+//                     var countTo = parseInt($this.text().replace(/\D/g, '')); // Получаем число для счетчика
+//                     $({ countNum: 0 }).animate({
+//                         countNum: countTo
+//                     },
+//                     {
+//                         duration: 1000,
+//                         easing: 'linear',
+//                         step: function() {
+//                             $this.text(Math.floor(this.countNum));
+//                         },
+//                         complete: function() {
+//                             $this.text(this.countNum);
+//                         }
+//                     });
+//                 });
+//             }
+//         });
+//     }).scroll();
+// });
+
+
+const refs2 = {
+  menuServ: document.querySelector("#menu-item-1"),
+  openServ: document.getElementById("menu-serv"),
+};
+
+let isMenuServOpen = false;
+
+const openMenuServ = () => {
+  refs2.openServ.classList.remove("is-hidden");
+  refs2.menuServ.classList.add("active");
+  isMenuServOpen = true;
+};
+
+const closeMenuServ = () => {
+  refs2.openServ.classList.add("is-hidden");
+  refs2.menuServ.classList.remove("active");
+  isMenuServOpen = false;
+};
+
+refs2.menuServ.addEventListener("mouseover", openMenuServ);
+refs2.menuServ.addEventListener("mouseleave", () => {
+  setTimeout(() => {
+    if (!refs2.menuServ.matches(":hover") && !refs2.openServ.matches(":hover")) {
+      closeMenuServ();
+    }
+  }, 100);
+});
+
+refs2.openServ.addEventListener("mouseover", openMenuServ);
+refs2.openServ.addEventListener("mouseleave", () => {
+  setTimeout(() => {
+    if (!refs2.menuServ.matches(":hover") && !refs2.openServ.matches(":hover")) {
+      closeMenuServ();
+    }
+  }, 100);
+});
+
+  $(document).ready(function(){
+    $('.pg-sheno-content__menu li').click(function(){
+      $('.pg-sheno-content__menu li').removeClass('active');
+      $(this).addClass('active');
+    });
+  });
